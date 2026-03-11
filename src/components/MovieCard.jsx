ff2b755493956612c5ceec0e312f58df3e48e0ad
Byun Kyung Min <bkm011123@gamil.com>
2026-03-11 13:00:58 +0900

    feat: 2단계 미션구현

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,32 +1,28 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-const imageUrl = "https://image.tmdb.org/t/p/w500";
-
 const MovieCardContainer = styled.section`
   overflow: hidden;
   display: flex;
   flex-direction: column;
+  width: 400px;
   border: 1px solid gray;
   border-radius: 10px;
-  background-color: black;
+  background-color: #202020;
   color: white;
   padding: 10px;
-  cursor: pointer;
 `;
 
-export const Card = ({ title, posterPath, voteAverage }) => {
+function MovieCard({ id, title, posterPath, voteAverage }) {
   const navigate = useNavigate();
 
   return (
-    <MovieCardContainer onClick={() => navigate("/details")}>
-      <img
-        style={{ width: `100%`, height: `auto`, display: `block` }}
-        src={imageUrl + posterPath}
-        alt={title}
-      />
-      <h4 style={{ margin: "10px 0" }}>{title}</h4>
-      <span style={{ fontSize: `15px` }}>평점: {voteAverage}</span>
+    <MovieCardContainer onClick={() => navigate(`/details/${id}`)}>
+      <img src={`https://image.tmdb.org/t/p/w500${posterPath}`} alt={title} />
+      <h3>{title}</h3>
+      <p>평점: {voteAverage}</p>
     </MovieCardContainer>
   );
-};
+}
+
+export default MovieCard;
